600c9bcf7db48e8849f806183f7f829b9f58ac0d
gaurav shah <gaurav.shah@neesum.com>
2022-08-10 17:05:09 +0100

    transient token progress

--- src/payment-api.js
+++ src/payment-api.js
@@ -13,22 +13,36 @@ export class PaymentApi {
       })
       .then(response => response.json())
       .then(jwt => {
-        console.log("jwt response: " + jwt);
+        // console.log("jwt response: " + jwt);
 
         return {
-
           jwtToken: jwt,
-
           // APIToken: apiToken,
-
-
         }
       })
       .catch(error => {
         console.log('There was an error');
         throw error;
       })
-
   }
+
+  postTransientToken(token) {
+    return paymentHttpClient.fetch("http://localhost:5005/cb/transient",
+      {
+        method: 'POST',
+        body: {
+          'token': token
+        }
+      })
+      .then(response => response.json())
+      .then(response => {
+        return response;
+      })
+      .catch(err =>{
+        console.log('There was an error posting transient token');
+        throw err;
+      })
+  }
+
 
 }
--- src/payment-page.js
+++ src/payment-page.js
@@ -7,21 +7,25 @@ export class PaymentPage {
   constructor(paymentApi) {
     this.paymentApi = paymentApi;
     this.paymentResponse;
-    this.judo;
   }
 
-  activate(params, routeConfig) {
-    this.routeConfig = routeConfig;
-    this.routeConfig.navModel.setTitle('Judopay payment')
-  }
 
-  receipt(){
+  receipt() {
+    console.log("calling receipt");
     const elem = document.getElementsByName("transientToken");
     console.log(elem);
 
   }
 
+  activate(params, routeConfig) {
+    console.log("calling activate")
+    this.routeConfig = routeConfig;
+    this.routeConfig.navModel.setTitle('CS Payment')
+  }
+
+
   attached() {
+    console.log("Calling attaching")
     let scriptElement = document.createElement('script');
     scriptElement.src = "https://flex.cybersource.com/cybersource/assets/microform/0.11/flex-microform.min.js";
     let captureContext = sessionStorage.cybersourceCaptureContext;
@@ -45,7 +49,8 @@ export class PaymentPage {
     number.load('#number-container');
     securityCode.load('#securityCode-container');
 
-    let form = document.querySelector('#my-form')
+
+    let form = document.querySelector('#cb-card-form')
     let payButton = form.querySelector('button');
     let errorsOutput = document.querySelector('#errors-output');
 
@@ -62,76 +67,92 @@ export class PaymentPage {
         } else {
           // At this point you may pass the token back to your server as you wish.
           // In this example we append a hidden input to the form and submit it.
-          const hiddenInput = document.createElement('input');
-          hiddenInput.setAttribute('type', 'hidden');
-          hiddenInput.setAttribute('name', 'transientToken');
-          hiddenInput.setAttribute('value', token);
-          form.appendChild(hiddenInput);
-          form.submit();
+          //todo this is not working. this.paymentApi is undefined at this stage
+          this.paymentApi.postTransientToken(token)
+            .then(response => {
+              console.log(response);
+            })
+            .catch(err => {
+              console.log("Error happened posting token " + err);
+            });
+          // const hiddenInput = document.createElement('input');
+          // hiddenInput.setAttribute('type', 'hidden');
+          // hiddenInput.setAttribute('name', 'transientToken');
+          // hiddenInput.setAttribute('value', token);
+          // form.appendChild(hiddenInput);
+          // form.submit();
+
+
         }
       });
     });
 
   }
 
-  processPayment() {
-    this.paymentApi.startPayment().then({});
 
+  detached() {
+    console.log("calling detached");
+    sessionStorage.removeItem("cybersourceCaptureContext");
   }
 
-  makePayment() {
+  // processPayment() {
+  //   this.paymentApi.startPayment().then({});
+  //
+  // }
 
-    console.log("Make Payment Called");
-
-    // Replace this value with the dynamic reference your receive from your SERVER API Call for the paymentSession
-
-    let paymentSession = sessionStorage.paymentSessionReference;
-    console.log("Payment Session : " + paymentSession);
-
-    // Payment Options (Must Match the paymentSession Call detail any mismatch will result in an error)
-
-    let configJSON = JSON.parse('{"judoId" : "'
-      + sessionStorage.paymentSessionApitoken
-      + '","amount" : "'
-      + window.amount
-      + '","currency" : "'
-      + window.currency
-      + '","yourConsumerReference" : "'
-      + sessionStorage.yourConsumerReference
-      + '","yourPaymentReference" : "'
-      + sessionStorage.yourPaymentReference + '"}');
-
-    console.log("Invoking Payment With Reference " + sessionStorage.paymentSessionReference);
-    console.log("configJSON = ");
-    console.table(configJSON);
-
-    // Invokes the Payment (SALE) - for PreAuth use invokePreauth instead (This will need collecting from the server)
-
-    this.judo.invokePayment(sessionStorage.paymentSessionReference, (configJSON))
-      .then((response) => {
-
-          // Received a Response
-
-          console.log("Response Received : " + JSON.stringify(response));
-          if (response.result === "Success" || response.result === "Declined" || response.result === "Retry") {
-            console.log("Response Received");
-            console.table(response);
-
-          } else {
-
-            // Response was not what was expected
-
-            console.log("Error : " + JSON.stringify(response))
-            console.table(response);
-
-          }
-        }
-      )
-      // Catch Any Error (Transaction Failed)
-      .catch((error) => {
-        console.log("ERROR CAUGHT " + JSON.stringify(error));
-      })
-  }
+  // makePayment() {
+  //
+  //   console.log("Make Payment Called");
+  //
+  //   // Replace this value with the dynamic reference your receive from your SERVER API Call for the paymentSession
+  //
+  //   let paymentSession = sessionStorage.paymentSessionReference;
+  //   console.log("Payment Session : " + paymentSession);
+  //
+  //   // Payment Options (Must Match the paymentSession Call detail any mismatch will result in an error)
+  //
+  //   let configJSON = JSON.parse('{"judoId" : "'
+  //     + sessionStorage.paymentSessionApitoken
+  //     + '","amount" : "'
+  //     + window.amount
+  //     + '","currency" : "'
+  //     + window.currency
+  //     + '","yourConsumerReference" : "'
+  //     + sessionStorage.yourConsumerReference
+  //     + '","yourPaymentReference" : "'
+  //     + sessionStorage.yourPaymentReference + '"}');
+  //
+  //   console.log("Invoking Payment With Reference " + sessionStorage.paymentSessionReference);
+  //   console.log("configJSON = ");
+  //   console.table(configJSON);
+  //
+  //   // Invokes the Payment (SALE) - for PreAuth use invokePreauth instead (This will need collecting from the server)
+  //
+  //   this.judo.invokePayment(sessionStorage.paymentSessionReference, (configJSON))
+  //     .then((response) => {
+  //
+  //         // Received a Response
+  //
+  //         console.log("Response Received : " + JSON.stringify(response));
+  //         if (response.result === "Success" || response.result === "Declined" || response.result === "Retry") {
+  //           console.log("Response Received");
+  //           console.table(response);
+  //
+  //         } else {
+  //
+  //           // Response was not what was expected
+  //
+  //           console.log("Error : " + JSON.stringify(response))
+  //           console.table(response);
+  //
+  //         }
+  //       }
+  //     )
+  //     // Catch Any Error (Transaction Failed)
+  //     .catch((error) => {
+  //       console.log("ERROR CAUGHT " + JSON.stringify(error));
+  //     })
+  // }
 
 
 }
